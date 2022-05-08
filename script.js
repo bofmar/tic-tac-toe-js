77e1891e459cddcfcf9e9a61e4a41790940f8cca
bofmar <bofmar@outlook.com>
2022-05-08 09:28:30 +0300

    Add ability for board module to display contents of board array

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const board = (()=>{
+  const boardArray = ["","","x","o","x","o","x","",""];
+
+  //DOM selectors
+  const htmlBoard = document.querySelector(".board");
+  const cells = document.querySelectorAll(".cell");
+
+  //Methods
+  function _display(){
+    boardArray.map((item, index) => {
+      if(item === "x"){
+        cells[index].classList.add("x");
+      }
+      if(item === "o"){
+        cells[index].classList.add("circle");
+      }
+    });
+  }
+
+  return {_display};
+})();
