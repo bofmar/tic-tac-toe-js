0154cb342a9bb0c97e1306a059db98f692a2ea66
bofmar <bofmar@outlook.com>
2022-05-08 10:01:00 +0300

    Add ability for player to make moves on the board

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const board = (()=>{
-  let boardArray = ["","","x","o","x","o","x","",""];
+  let boardArray = ["","","","","","","","",""];
+  let currentPlayer = "x";
 
   //DOM selectors
   const htmlBoard = document.querySelector(".board");
   const cells = document.querySelectorAll(".cell");
+
+  //Event Binders
+  cells.forEach(cell => cell.addEventListener("click",_commitMove));
 
   //Methods
   function _display(){
@@ -17,16 +21,45 @@ const board = (()=>{
     });
   }
 
-  function reset(){
+  function _reset(){
     boardArray = ["","","","","","","","",""];
     for(let i = 0; i < cells.length; i++){
       cells[i].classList.remove("x");
       cells[i].classList.remove("circle");
     }
+    currentPlayer = "x";
+  }
+
+  function _commitMove(e){
+    if(currentPlayer === "x"){
+      e.target.classList.add("x");
+      boardArray[e.target.dataset.index] = "x";
+    }
+    else{
+      e.target.classList.add("circle");
+      boardArray[e.target.dataset.index] = "o";
+    }
+    _changePlayer();
+  }
+
+  function _changePlayer(){
+    if(currentPlayer === "x"){
+      currentPlayer = "o";
+      htmlBoard.classList.add("circle");
+      htmlBoard.classList.remove("x");
+    }
+    else{
+      currentPlayer = "x";
+      htmlBoard.classList.add("x");
+      htmlBoard.classList.remove("circle");
+    }
+  }
+
+  function getBoard(){
+    return boardArray;
   }
 
   return {
-    _display,
-    reset
+    getBoard
   };
 })();
